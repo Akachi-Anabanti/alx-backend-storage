da820bd5a29ef2bdd2a51dd776794245bcc8293c
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-15 19:35:13 +0000

    nginx log parse function

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,21 +2,33 @@
 """A script that describes nginx logs"""
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.logs
 
-print(f'{db.nginx.count()} logs')
+def log_parsing():
+    """Parses nginx logs"""
+    # connects to the mongoclient
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs  # establishes a connection tho the db
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    # prints the total document count
+    print(f'{db.nginx.count()} logs')
 
-print("Methods:")
-for method in methods:
-    method_count = db.nginx.count_documents({'method': method})
-    print(f'\tmethod {method}: {method_count}')
+    # set of methods to check
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    # loops to count the document
+    print("Methods:")
+    for method in methods:
+        method_count = db.nginx.count_documents({'method': method})
+        print(f'\tmethod {method}: {method_count}')
+
+    # counts the paths to /status
+    status_paths = db.nginx.count_documents(
+            {'method': 'GET', 'path': '/status'}
+            )
+
+    # prints the total number of paths
+    print(f'{status_paths} status check')
 
 
-status_paths = db.nginx.count_documents(
-        {'method': 'GET', 'path': '/status'}
-        )
-
-print(f'{status_paths} status check')
+if __name__ == "__main__":
+    log_parsing()
