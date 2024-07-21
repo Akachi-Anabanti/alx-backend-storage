3c0233e6597a1f0795bde1717ef6bd918a5de515
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-21 01:01:42 +0000

    retrieve from the database

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 import redis
 import uuid
 import json
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -21,3 +21,17 @@ class Cache:
         id = uuid.uuid4().hex
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str, fn: Optional[Callable]=None):
+        """Retrieves the value of the key from db
+            and converts to original format"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, data: str) -> str:
+        """converts a string"""
+        return data.decode('utf-8', 'strict')
+
+    def get_int(self, data: str) -> int:
+        """Returns int value of decode byte"""
+        return int(data)
