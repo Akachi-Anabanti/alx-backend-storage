f63ab3e9c855085935000f1fc9f746db2d3bee2c
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-21 00:33:12 +0000

    fixed type annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 import redis
 import uuid
 import json
-from typing import Any
+from typing import Union
 
 
 class Cache:
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes data and stores  in redis
         returns : Data ID as str"""
 
