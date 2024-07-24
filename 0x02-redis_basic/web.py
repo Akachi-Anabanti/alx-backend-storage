7d1f683de1f25ec2caf33973802103b125bb3634
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-24 09:25:27 +0000

    caching web requests and counting

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""A script that implements a web cache tracker"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+r = redis.Redis()
+
+
+def cache_count(expiration: int = 10):
+    def decorator(method: Callable) -> Callable:
+        """decorator monitors the call count"""
+        @wraps(func)
+        def wrapper(url: str):
+            count_key = f"count:{url}"
+            r.incr(count_key)  # increment the count key
+            # check if the cached content already exists and return it instead
+            cached_content = r.get(count_key)
+            if cached_content:
+                return cached_content.decode("utf-8")
+            # else call the method with the url
+            content = func(url)
+            # set the expiration and the content
+            r.setx(count_key, expiration, content)
+            return content
+        return wrapper
+    return decorator
+
+
+@cache_count()
+def get_page(url: str) -> str:
+    """Uses the request module to get the HTML content
+    of a url and keeps count of the number of requests made"""
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == "__main__":
+    url = "https://google.com/"
+    content = get_page(url)
+    print(f"Access count: {r.get(f'count:{url}').decode('utf-8')}")
